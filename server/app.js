eb95d59c77c080594c87cbc7e3e898c54b906a6c
Ellis Tsung <el1t@ymail.com>
2016-03-24 23:13:40 -0400

    Switch to koa

--- server/app.js
+++ server/app.js
@@ -1,58 +1,34 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
+let koa = require('koa'),
+	router = require('koa-router')(),
+	logger = require('koa-logger'),
+	serve = require('koa-static'),
+	bodyParser = require('koa-bodyparser'),
+	path = require('path'),
+	app = koa();
 
-var routes = require('./routes/index');
-
-var app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, '../dist', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../dist')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+// Handle errors
+app.use(function *(next) {
+	try {
+		yield next;
+	} catch (err) {
+		this.status = err.status || 500;
+		this.body = err.message;
+		this.app.emit('error', err, this);
+	}
 });
 
-// error handlers
+// Add middleware
+app.use(logger());
+app.use(bodyParser());
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
+// Serve static files
+app.use(serve(path.join(__dirname, '../dist')));
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
+// router.get('/', function *() {
+// 	this.body = 'Hey there';
+// });
 
+// app.use(router.routes());
 
 module.exports = app;
