819b8a7653fdb99c6dbd590e981a670348534290
Ellis Tsung <git@tsung.co>
2017-03-14 21:48:54 -0400

    Setup github pages publishing

--- run.js
+++ run.js
@@ -14,13 +14,14 @@ const fs = require('fs');
 const del = require('del');
 const ejs = require('ejs');
 const webpack = require('webpack');
+const path = require('path');
 
 // TODO: Update configuration settings
 const config = {
-	title: 'React Static Boilerplate',        // Your website title
-	url: 'https://rsb.kriasoft.com',          // Your website URL
-	project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
-	trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
+	title: 'CNS@UVA',        // Your website title
+	url: 'https://cnsuva.github.com',          // Your website URL
+	project: 'cns-at-uva',      // Firebase project. See README.md -> How to Deploy
+	trackingID: 'UA-84953617-2',               // Google Analytics Site's ID
 };
 
 const tasks = new Map(); // The collection of automation tasks ('clean', 'build', 'publish', etc.)
@@ -96,16 +97,43 @@ tasks.set('build', () => {
 // Build and publish the website
 // -----------------------------------------------------------------------------
 tasks.set('publish', () => {
-	const firebase = require('firebase-tools');
-	return run('build')
-		.then(() => firebase.login({ nonInteractive: false }))
-		.then(() => firebase.deploy({
-			project: config.project,
-			cwd: __dirname,
-		}))
-		.then(() => {
-			setTimeout(() => process.exit());
+	const remote = {
+		url: 'https://github.com/cnsuva/cnsuva.github.io.git',
+		branch: 'gh-pages',
+	};
+	global.DEBUG = process.argv.includes('--debug') || false;
+	const spawn = require('child_process').spawn;
+	const opts = { cwd: path.resolve(__dirname, './public'), stdio: ['ignore', 'inherit', 'inherit'] };
+	const git = (...args) => new Promise((resolve, reject) => {
+		spawn('git', args, opts).on('close', code => {
+			if (code === 0) {
+				resolve();
+			} else {
+				reject(new Error(`git ${args.join(' ')} => ${code} (error)`));
+			}
 		});
+	});
+
+	return Promise.resolve()
+		.then(() => run('clean'))
+		.then(() => git('init', '--quiet'))
+		.then(() => git('config', '--get', 'remote.origin.url')
+			.then(() => git('remote', 'set-url', 'origin', remote.url))
+			.catch(() => git('remote', 'add', 'origin', remote.url))
+		)
+		.then(() => git('ls-remote', '--exit-code', remote.url, 'master')
+			.then(() => Promise.resolve()
+				.then(() => git('fetch', 'origin'))
+				.then(() => git('reset', `origin/${remote.branch}`, '--hard'))
+				.then(() => git('clean', '--force'))
+			)
+			.catch(() => Promise.resolve())
+		)
+		.then(() => run('build'))
+		.then(() => git('add', '.', '--all'))
+		.then(() => git('commit', '--message', new Date().toUTCString())
+			.catch(() => Promise.resolve()))
+		.then(() => git('push', 'origin', `HEAD:${remote.branch}`, '--force', '--set-upstream'));
 });
 
 //
